9ee1049120dd8f9844f4513e1761e8ab0b108c84
stachecoding2 <stachecoding2@DESKTOP-0BN2DDD.localdomain>
2022-08-16 12:13:11 -0400

    user session and error reducers implemented configureStore complete default state in browser

--- frontend/table_prete.jsx
+++ frontend/table_prete.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { login, logout, signup } from './util/session_api_util';
+import configureStore from './store/store';
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    const store = configureStore()
 
     const root = document.getElementById('root');
 
     window.login = login;
     window.logout = logout;
     window.signup = signup;
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
 
 
     ReactDOM.render(<h1>Welcome to Table Prete</h1>, root)
